026a538d442c2a84cefa9596dc24059ea04f91df
LioKor <support@liokor.com>
2019-03-18 05:16:05 +0300

    Feature: deleting of note; Refactoring;

--- editnote.php
+++ editnote.php
@@ -6,7 +6,6 @@
 	<link rel="stylesheet" href="css/bootstrap.min.css">
 	<link rel="stylesheet" href="css/font-awesome.min.css">
 </head>
-
 <body class="bg-light">
 	<div class="container">
 		<h1 class="my-4 text-center"><i class="fa fa-file-text-o" aria-hidden="true"></i> Мои записки</h1>
@@ -17,29 +16,29 @@
 		</div>
 
 		<div class="card">
-      <?php
-          $db = new PDO('mysql:host=localhost;dbname=notesproject', 'root', '');
+			<?php
+				$db = new PDO('mysql:host=localhost;dbname=notesproject', 'root', '');
 
-          $query = $db->prepare('SELECT title, content FROM notes WHERE id = ?;');
-          $query->execute(array($_GET['id']));
+				$query = $db->prepare('SELECT title, content FROM notes WHERE id = ?;');
+				$query->execute(array($_GET['id']));
 
-          $result = $query->FETCHALL(PDO::FETCH_ASSOC);
-          $e = $result[0];
-      ?>
+				$result = $query->FETCHALL(PDO::FETCH_ASSOC);
+				$e = $result[0];
+			?>
 			<div class="card-body">
 				<form method="POST" action="shownote.php?id=<?php echo $_GET['id']; ?>">
 					<input type="hidden" name="edit_id" value="<?php echo $_GET['id']; ?>">
 
-            <div class="form-group">
-                <input type="text" name="edit_title" value="<?php echo $e['title']; ?>" class="form-control">
-            </div>
-            <div class="form-group">
-                <textarea name="edit_content" class="form-control" rows="7"><?php echo $e['content']; ?></textarea>
-            </div>
-            <div class="from-group">
-                <input type="submit" class="btn btn-success" value="Сохранить">
-            </div>
-        </form>
+		            <div class="form-group">
+		                <input type="text" name="edit_title" value="<?php echo $e['title']; ?>" class="form-control">
+		            </div>
+		            <div class="form-group">
+		                <textarea name="edit_content" class="form-control" rows="15"><?php echo $e['content']; ?></textarea>
+		            </div>
+		            <div class="from-group">
+		                <input type="submit" class="btn btn-success" value="Сохранить">
+		            </div>
+        		</form>
 			</div>
 		</div>
 	</div>
--- index.php
+++ index.php
@@ -15,16 +15,38 @@
 			<a class="btn btn-primary" href="addnote.php"><i class="fa fa-plus" aria-hidden="true"></i> Добавить записку</a>
 		</div>
 
-		<div class="list-group">
-	    <?php
-	        $db = new PDO('mysql:host=localhost;dbname=notesproject', 'root', '');
-	        $query = $db->query('SELECT * FROM notes;');
-	        $result = $query->FETCHALL(PDO::FETCH_ASSOC);
+		<?php
+			$db = new PDO('mysql:host=localhost;dbname=notesproject', 'root', '');
 
-	        foreach ($result as $e) {
-	            echo '<a href="shownote.php?id='.$e['id'].'" class="list-group-item list-group-item-action">'.$e['title'].'</a>';
-	        }
-	    ?>
+			// Добавление записки
+			if (isset($_POST['title']) && isset($_POST['content'])) {
+				$title = strip_tags($_POST['title']);
+				$content = strip_tags($_POST['content']);
+
+				$query = $db->prepare('INSERT INTO notes (title, content) VALUES (?, ?);');
+				$query->execute(array($title, $content));
+
+				echo '<div class="alert alert-success">Записка успешно добавлена!</div>';
+			}
+
+			// Удаление записки
+			if (isset($_POST['delete_id'])) {
+				$query = $db->prepare('DELETE FROM notes WHERE id = ?;');
+				$query->execute(array($_POST['delete_id']));
+
+				echo '<div class="alert alert-warning">Записка успешно удалена!</div>';
+			}
+		?>
+
+		<div class="list-group">
+		    <?php
+		        $query = $db->query('SELECT * FROM notes;');
+		        $result = $query->FETCHALL(PDO::FETCH_ASSOC);
+
+		        foreach ($result as $e) {
+		            echo '<a href="shownote.php?id='.$e['id'].'" class="list-group-item list-group-item-action">'.$e['title'].'</a>';
+		        }
+		    ?>
 		</div>
 	</div>
 </body>
